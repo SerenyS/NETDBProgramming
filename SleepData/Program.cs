2867426dc27fd7eaa31de4bc119098c398674d4b
Sereny Sanchez <52845840+SerenyS@users.noreply.github.com>
2020-02-10 21:31:39 -0600

    Completed String Interpolation

--- SleepData/Program.cs
+++ SleepData/Program.cs
@@ -12,15 +12,14 @@ namespace SleepData
             Console.WriteLine("Enter 2 to parse data.");
             Console.WriteLine("Enter anything else to quit.");
             // input response
-            string resp = Console.ReadLine();
-
+            string resp = Console.ReadLine()
+;
             // specify path for data file
-            string file = "/users/jgrissom/downloads/data.txt";
+           string file = AppDomain.CurrentDomain.BaseDirectory +"data.txt";
 
             if (resp == "1")
             {
-                // create data file
-
+              
                 // ask a question
                 Console.WriteLine("How many weeks of data is needed?");
                 // input the response (convert to int)
@@ -32,6 +31,11 @@ namespace SleepData
                 DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
                 // subtract # of weeks from endDate to get startDate
                 DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
+               
+
+
+
+
 
                 // random number generator
                 Random rnd = new Random();
@@ -50,7 +54,9 @@ namespace SleepData
                     }
                     // M/d/yyyy,#|#|#|#|#|#|#
                     //Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
-                    sw.WriteLine($"{dataDate:M/d/yyyy},{string.Join("|", hours)}");
+
+                    
+                  sw.WriteLine($"{dataDate:dddd dd MMMM},{string.Join("|", hours)}");
                     // add 1 week to date
                     dataDate = dataDate.AddDays(7);
                 }
@@ -58,9 +64,37 @@ namespace SleepData
             }
             else if (resp == "2")
             {
-                // TODO: parse data file
+                StreamReader fileReader = new StreamReader(file);
+                while (!fileReader.EndOfStream)
+                        {
+                   
+                    string line = fileReader.ReadLine();
 
-            }
+                    Console.WriteLine(line);
+
+
+                    String[] laFecha = line.Split(',');
+                    DateTime parsedDate = DateTime.Parse(laFecha[0]);
+                    String semana = (parsedDate.ToString("dddd dd MMMM"));
+
+                    String[] diasSemana = laFecha[1].Split(',','|'); // Why is the comma splitting mandatory, when youre already splitting the array?
+
+                    
+
+                  Console.WriteLine("Week of {0}",semana);
+                 
+                   Console.WriteLine($"{"Mon",4}{"Tue",4}{"Wed",4}{"Thr",4}{"Fri",4}{"Sat",4}{"Sun",4}");
+                   Console.WriteLine($"{diasSemana[0],4}{diasSemana[1],4}{diasSemana[2],4}{diasSemana[3],4}{diasSemana[4],4}{diasSemana[5],4}{diasSemana[6],4}");
+
+                    
+                
+
+                
+                }
+                    }
+                  
+
+            
         }
     }
 }
--- YourName/Program.cs
+++ YourName/Program.cs
@@ -13,6 +13,57 @@ namespace YourName
             // display the string variable
             Console.WriteLine("Hello, " + name);
             Console.WriteLine("Hello, {0}", name);
+
+
+            Phone phone = new Phone();
+
+            phone.Number = 9204715179;
+
+
+
+
+        }
+
+
+        public class Phone
+        {
+            public long Number { get; set; }
+
+            public string Color { get; set; }
+
+            public string Manufacturer { get; set; }
+
+            //ctor tab tab
+
+            public Phone()// constructor no return type
+            {
+
+            }
+
+            public Phone (int number, string manu, string color) // passing and setting values
+            {
+                Number = number;
+                Manufacturer = manu;
+                Color = color;
+            }
+
+            //methods (functionality)
+
+            public void ReadFile() { }
+
+            public void WriteFile() { }
+
+            public void CheckInput() { }
+
+
+            //Properties = What a properties Needs to have to be a ticket
+            //methods = functionality or "actions" needed such as verify information
+            // interface = what the user gets to use and see and interact with
+            //extends = ':'
+            //override  = changes to make it default behavior
+            //base = base class - object is a class
+
         }
     }
+
 }
